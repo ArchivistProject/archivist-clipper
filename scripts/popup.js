e341c0da7a1c905e0ef00dbee0c8f433b4bcbac0
Rob Edwards <redwards1934@gmail.com>
2017-01-22 20:00:58 -0500

    Generate the form data from sample json object

--- scripts/popup.js
+++ scripts/popup.js
@@ -32,7 +32,7 @@ function click(e) {
 
         if (this.status == 200) {
             var data = JSON.parse(this.responseText);
-            alert(data);
+
             // we get the returned data
         }
 
@@ -40,9 +40,59 @@ function click(e) {
     };
 }
 
+function getMetadataFields(){
+    return ([
+                { name: "Author", type: "text" },
+                { name: "Title", type: "text" }
+           ]);
+}
+
+function generateMetadataInput(metadataField){
+    var inputTemplate = document.querySelector('#input-template');
+    var label = inputTemplate.content.querySelector('label');
+    var input = inputTemplate.content.querySelector('input');
+
+    label.innerText = metadataField["name"];
+    label.htmlFor = metadataField["id"];
+
+    input.id = metadataField["id"];
+    input.name = metadataField["name"];
+    input.type = metadataField["type"];
+
+    var clone = document.importNode(inputTemplate.content, true); // Deep copy of DOM
+
+    return clone;
+}
+
+function generateMetadataInputs(metadataJson){
+    var metadataInputs = [];
+
+    metadataJson.forEach(function(metadataField){
+       var id = metadataField.name.toLowerCase();
+       metadataField["id"] = id;
+
+        var fieldHtml = generateMetadataInput(metadataField);
+        metadataInputs.push(fieldHtml);
+    })
+
+    return metadataInputs;
+}
+
+function addMetadataInputs(metadataInputs){
+    var form = document.getElementById("metadata-form");
+
+    for(var i = 0; i < metadataInputs.length; i++){
+        form.insertBefore(metadataInputs[i], form.firstChild);
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function () {
-    // Find button
+    // Find button and add click event
     var savePageBtn = document.getElementById("save-page-btn");
     savePageBtn.addEventListener('click', click);
 
+    // Generate and add input fields for HTML form
+    var metadataFields = getMetadataFields();
+    var metadataInputs = generateMetadataInputs(metadataFields);
+    addMetadataInputs(metadataInputs);
 });
