30a83a9fcd7c2b0e26915bed7a5f432def9b7f96
Charles <gilliamcc@gmail.com>
2017-03-12 23:36:45 -0400

    Use the api token when making requests

--- scripts/archivist/api.js
+++ scripts/archivist/api.js
@@ -1,22 +1,28 @@
 Archivist.api = {};
 
 Archivist.api.getMetadataFieldGroups = (successHandler) => {
-  $.ajax({
-    url: 'http://localhost:3000/public/groups',
-    type: 'GET',
-    success: successHandler,
-    contentType: 'application/json',
-    dataType: 'json',
+  chrome.storage.sync.get('api_key', (s) => {
+    $.ajax({
+      url: 'https://localhost:3000/public/groups',
+      type: 'GET',
+      success: successHandler,
+      contentType: 'application/json',
+      dataType: 'json',
+      headers: { Authorization: s.api_key },
+    });
   });
 };
 
 Archivist.api.postDocumentData = (documentData, successHandler) => {
-  $.ajax({
-    url: 'http://localhost:3000/public/documents',
-    type: 'POST',
-    data: JSON.stringify(documentData),
-    success: successHandler,
-    contentType: 'application/json',
-    dataType: 'json',
+  chrome.storage.sync.get('api_key', (s) => {
+    $.ajax({
+      url: 'https://localhost:3000/public/documents',
+      type: 'POST',
+      data: JSON.stringify(documentData),
+      success: successHandler,
+      contentType: 'application/json',
+      dataType: 'json',
+      headers: { Authorization: s.api_key },
+    });
   });
 };
