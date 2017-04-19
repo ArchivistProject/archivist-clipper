54789eb3485b327cd9f1e9df17b367d6d8a4f67d
Robert Edwards <me@robertedwards.rocks>
2017-04-18 20:52:46 -0400

    Add api location as option and use it in GET/POST

--- scripts/archivist/base.js
+++ scripts/archivist/base.js
@@ -1,5 +1,6 @@
 const Archivist = {
   metadataFields: [],
+
   getInputDateFormat: (date) => {
     const paddedMonth = (`0${date.getMonth() + 1}`).slice(-2);
     const paddedDate = (`0${date.getDate()}`).slice(-2);
